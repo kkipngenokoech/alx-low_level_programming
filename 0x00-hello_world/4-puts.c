78ae32f17afd93aed887a9fa4ec67f7db6ac396e
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-06-30 06:12:07 -0700

    added the betty coding style-description of the main program and sorrounding the return with paranthesi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/*
+ * main-entry point
+ * return: always 0-always success
+ * */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
