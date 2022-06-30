bf1ac0a8a1833269e333da0de7c5e1a94d0f703c
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-06-30 06:22:09 -0700

    still fixing the betty naming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /*
- * main-entry point
- * return: always 0-always success
- * */
+ * main -Entry point
+ * Return: always 0-always success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
