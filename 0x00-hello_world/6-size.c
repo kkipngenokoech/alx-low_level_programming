8627f4f99f0b9f0e71b5fb33c447c5860f672eca
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-06-30 10:22:06 -0700

    fixing betty coding style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,11 @@ int main(void)
 	long long int integerc;
 	char charactera;
 	float floata;
-	printf("size of a char: %lu byte(s)\n",(unsigned long)sizeof(charactera));
-	printf("size of an int: %lu byte(s)\n",(unsigned long)sizeof(integera));
-	printf("size of a long int: %lu byte(s)\n",(unsigned long)sizeof(integerb));
-	printf("size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(integerc));
-	printf("size of a float: %lu byte(s)\n",(unsigned long)sizeof(floata));
+
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(charactera));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(integera));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(integerb));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(integerc));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(floata));
 	return (0);
 }
