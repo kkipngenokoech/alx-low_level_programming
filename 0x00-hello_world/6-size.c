9a11bce6f379e8e8c06d62c92d67fe63e5ad5ab0
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-06-30 10:01:24 -0700

    fixed the output

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 	printf("The size of a char:%lu byte(s)\n",(unsigned long)sizeof(charactera));
 	printf("The size of an int:%lu byte(s)\n",(unsigned long)sizeof(integera));
 	printf("The size of a float:%lu byte(s)\n",(unsigned long)sizeof(floata));
-	printf("The size of a long int:%lu byte(s)",(unsigned long)sizeof(integerb));
+	printf("The size of a long int:%lu byte(s)\n",(unsigned long)sizeof(integerb));
 	printf("The size of a long long integerc:%lu byte(s)",(unsigned long)sizeof(integerc));
 	return (0);
 }
