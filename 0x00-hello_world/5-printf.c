4973b66cdf5955e0aaec7123ede4c3ccfc74597f
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-06-30 09:20:37 -0700

    fixing the betty coding style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
- * main - Entry point
+/**
+ *  main - Entry point
  *
- * Return: Always o (success)
+ *  Return: Always o (success)
  */
- int main (void)
+ int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
