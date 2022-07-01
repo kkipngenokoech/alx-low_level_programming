c048da58fd319781ed9435b12d922b34857bf962
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-01 07:17:21 -0700

    printing base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - printing hexadecimal in lowercase
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'a'; i <= 'f'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
