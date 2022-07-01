f459bd3c7d1db5c935e4e2a205a4d4abdbcba93b
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-01 06:33:42 -0700

    betty style

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main 
+ * main - Prints the alphabet except the letters q and e.
  *
- * Return:
+ * Return: 0 on success
  */
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z' ; ch++)
 	{
 		if (ch == 'q' || ch == 'e')
