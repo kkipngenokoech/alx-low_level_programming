9e77315ddbc6e34c5c5ab88c9eefcb1229af3b44
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-01 10:52:28 -0700

    fixing stuff

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,16 +7,16 @@
 int main(void)
 {
 	int index;
-	int innerindex;
+	int innerindex, innerindexx;
 
 	innerindex = '0';
 	for (index = '0'; index <= '9'; index++)
 	{
-		for (int innerindexx = innerindex; innerindexx <= '9'; innerindexx++)
+		for (innerindexx = innerindex; innerindexx <= '9'; innerindexx++)
 		{
 			putchar(index);
 			putchar(innerindexx);
-			while  (innerindexx != '9' && innerindexx != '9')
+			if  (innerindexx != '9' && innerindexx != '9')
 			{
 				putchar(',');
 				putchar(' ');
