512fae7628b522947bb77e45f26a3c687ac06ee3
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-01 04:41:19 -0700

    fixing the betty coding style

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
+
+/**
+ * main - prints out lowercase letters from a to z
+ *
+ * Return: 0 always (success)
+ */
 int main(void)
 {
 	char ch;
-	for( ch='a';ch<'z';ch++)
+	for(ch='a';ch<='z';ch++)
 	{
 		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
-}	
+}
