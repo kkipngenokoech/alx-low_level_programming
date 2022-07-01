750593bf574aba4a9211b68df3fbdbeacb685214
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-01 06:17:56 -0700

    excluding specific alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main 
+ *
+ * Return:
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z' ; ch++)
+	{
+		if (ch == 'q' || ch == 'e')
+		{
+			continue;
+			putchar(ch);
+		}
+	}
+	return (0);
+
