0c2ae14cfb47bb682fb6d58d4ad02d392271994f
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-01 05:52:18 -0700

    adding betty coding atyle

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
+/*
+ * main - prints out lowercase and uppercase letters
+ *
+ * return: 0 always (success)
+ */
 int main(void)
 {
 	char ch;
-	for(ch='a';ch<='z';ch++)
+
+	for (ch ='a';ch < ='z';ch++)
 	{
 		putchar(ch);
 	}
-	for(ch='A';ch<='Z';ch++)
+	for (ch = 'A';ch <= 'Z';ch++)
 	{
 		putchar(ch);
 	}
