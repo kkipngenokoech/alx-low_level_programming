c9feb62c2dc2a93afcccbcf656854f1bf7fee687
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-05 03:11:27 -0700

    adding parameter c well

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,9 +4,9 @@
 /**
  * _islower - prints out 1 if the lettter passed is lowercase else 0
  *
- * @c -parameter being passed in presumed a letter
+ * @c: -parameter being passed in presumed a letter
  * Return: 1 on if lowercase else 0
- */
+ */:
 int _islower(int c)
 {
 	char character;
