3722fbcd09af3f9d52f381d9468b02455e35b54c
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-05 06:00:12 -0700

    printing jack baeurs hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of jack bauer minutes
+ */
+void jack_bauer(void)
+{
+	int hour;
+	int minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar(hour);
+			_putchar(':');
+			_putchar(minute);
+			_puchar('\n');
+		}
+	}
+}
