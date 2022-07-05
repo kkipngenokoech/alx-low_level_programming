b6124356af748a20f02ea6137d586f61e0aae9d9
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-05 05:22:29 -0700

    new file , absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - it computes the absolute value of an integrer
+ * @n: the parameter to be passed
+ * Return: 0 always on success
+ */
+int _abs(int n)
+{
+	int number;
+
+	number = n;
+	if (number < 0)
+	{
+		return (number*(-1));
+	}
+	else
+	{
+		return (number);
+	}
+}
