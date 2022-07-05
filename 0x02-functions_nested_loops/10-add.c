594d89f4f70a2a78505a011b7e1f5940dfda2972
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-05 09:30:47 -0700

    adding numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - adds two numbers
+ * @n: first parameter to be passed
+ * @m: second parameter to be passed
+ * Return: the sum
+ */
+int add(int n, int m)
+{
+	int first;
+	int second;
+	int sum;
+
+	first = n;
+	second = m;
+	sum = first + second;
+	return (sum);
+}
