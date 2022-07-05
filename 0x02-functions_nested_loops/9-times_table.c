ac41bb082d9197c9408aef75584806e1af1b05a2
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-05 07:16:22 -0700

    fixing stuff

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,25 +5,43 @@
  */
 void times_table(void)
 {
-	int index;
-	int row;
-
-	for (row = 0; row <= 9; row++)
+	void times_table(void)
 	{
-		for (index = 0; index <= 9; index++)
+		int a;
+		int b;
+		int c;
+		for (a = 0; a <= 9; a++)
 		{
-			_putchar(index*row);
-			if (index == 9)
+			for (b = 0; b <= 9; b++)
 			{
-				_putchar('$');
-				_putchar('\n');
+				c = a * b;
+				if ((c / 10) == 0)
+				{
+					if (b == 0)
+					{
+						_putchar ('0');
+					}
+					if (b != 0)
+					{
+						_putchar (' ');
+						_putchar ((c % 10) + '0');
+					}
+					if (b < 9)
+					{
+						_putchar(',');
+						_putchar (' ');
+					}
+				}
+				else
+				{
+					_putchar ((c / 10) + '0');
+					_putchar ((c % 10) + '0');
+					if (b < 9)
+					{
+						_putchar(',');
+						_putchar (' ');
+					}
+				}
 			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar ('\n');
 		}
-	}
-}
-
