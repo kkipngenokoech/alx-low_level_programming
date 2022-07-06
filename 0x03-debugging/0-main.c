b381f203f0ac76b7fc47d13c4b8c9567f98d8757
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-06 10:18:14 -0700

    removed the stdio header in the 0-main file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * main - tests if the positive or negative funcctions works
@@ -7,8 +6,9 @@
 int main(void)
 {
 	int index;
-
 	index = 0;
+	
 	postive_or_negative(index);
+
 	return (0);
 }
