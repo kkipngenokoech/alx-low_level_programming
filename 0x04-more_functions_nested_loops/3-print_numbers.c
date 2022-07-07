65b573ff91245ca262a57076883e6cbc7e1bb0de
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-07 05:55:35 -0700

    printing from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers -  prints numbers ranging from 0 to 1
+ */
+void print_numbers(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		_putchar(number);
+	}
+}
