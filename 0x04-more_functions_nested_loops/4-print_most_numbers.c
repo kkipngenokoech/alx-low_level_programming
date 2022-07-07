11095066d031bd434abd04804d85a2c990d283ad
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-07 09:05:07 -0700

    excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - printing out numbers from 0 to 9 but skips 2 and 4
+ */
+void print_most_numbers(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		if (!(ch == '2' || ch == '4'))
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
