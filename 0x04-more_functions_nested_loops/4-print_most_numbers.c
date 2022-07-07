a5c00b18ebb62151a85d258180e0bd5e73f15f52
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-07 09:35:51 -0700

    betty coding style

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,8 +2,8 @@
 #include "main.h"
 
 /**
- * print_most_numbers - printing out numbers from 0 to 9 but skips 2 and 4 nums
- * Do not print 2 and 4
+ * print_most_numbers - printing out numbers from 0 to 9 but skips 2 and 4
+ * Do not print 2 and 4 nums
  * followed by a new line
  * Return: always 0
  */
@@ -13,7 +13,7 @@ void print_most_numbers(void)
 
 	for (ch = '0'; ch <= '9'; ch++)
 	{
-		if (ch != '2' && ch != '4')
+		if (!(ch == '2' || ch == '4'))
 		{
 			_putchar(ch);
 		}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+/**
+ * more_numbers -  function that prints 10 times number fro 0 to 14
+ * 
