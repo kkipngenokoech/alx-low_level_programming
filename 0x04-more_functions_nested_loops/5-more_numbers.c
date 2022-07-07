ce3668059db64e418e370c2c69f9bf575e30a6eb
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-07 09:44:50 -0700

    printing 20 times to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,4 +2,22 @@
 
 /**
  * more_numbers -  function that prints 10 times number fro 0 to 14
- * 
+ * Return zero
+ */
+void more_numbers(void)
+{
+	int number, row;
+
+	for (row = 0; row < 10; row++)
+	{
+		for (number = 0; number < 15; number++)
+			{
+				if (ch >= 10)
+				{
+					_putchar((ch / 10) + 48);
+				}
+				_putchar((ch % 10) + 48);
+			}
+		_putchar('\n');
+	}
+}
