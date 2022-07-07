88aaa12b1711f15dae59cb9a12eb32402235d302
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-07 00:30:01 -0700

    betty coding style

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,14 @@
 
 /**
  * main - tests if the positive or negative funcctions works
- * return: always zero on success
+ * Return: always zero on success
  */
 int main(void)
 {
 	int index;
+
 	index = 0;
 	
 	positive_or_negative(index);
-
 	return (0);
 }
