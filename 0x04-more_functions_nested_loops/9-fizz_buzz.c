28a8ad04028e7e8e75b2e571ea67dda8a33bfc05
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-07 11:57:27 -0700

    printing buzzfizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - returns fizz for 3 else if buzz for 5 and fizzbuzz for 3 and 5
+ * Return: 0 for always true
+ */
+int main(void)
+{
+	int index;
+
+	for (index = 1; index <= 100; index ++)
+	{
+		if (index % 3 == 0 && index % 5 == 0)
+		{
+			printf("fizzbuzz ");
+		}
+		else if ( index % 3 == 0)
+		{
+			printf("fizz ");
+		}
+		else if (index % 5 == 0)
+		{
+			if (index == 100)
+				printf("buzz\n");
+			else
+			{
+				printf("buzz ");
+			}
+		}
+		else
+		{
+			printf("%d ",index);
+		}
+		
+	}
+}
