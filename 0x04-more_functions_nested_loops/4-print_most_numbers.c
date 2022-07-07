59c60f8a4a1ecbdafe633136b8a49abdb791f90f
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-07 09:14:17 -0700

    betty coding style

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,6 +3,9 @@
 
 /**
  * print_most_numbers - printing out numbers from 0 to 9 but skips 2 and 4 nums
+ * Do not print 2 and 4
+ * followed by a new line
+ * Return: always 0
  */
 void print_most_numbers(void)
 {
@@ -10,7 +13,7 @@ void print_most_numbers(void)
 
 	for (ch = '0'; ch <= '9'; ch++)
 	{
-		if ( ! (ch == '2' || ch == '4'))
+		if (!(ch == '2' || ch == '4'))
 		{
 			_putchar(ch);
 		}
