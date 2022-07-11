f0f8b26447d82a4735fa3888c5cc9058b4284f96
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-11 03:40:27 -0700

    printing in shifts of two

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - prints the string found after every 2 string characters
+ * @str: string to be dissected
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int counter;
+
+	counter = 0;
+	while (str[counter] != '\0')
+	{
+		_putchar(str[counter]);
+		counter +=2;
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ void _puts(char *str);
 int _putchar(char);
 void rev_string(char *s);
 void print_rev(char *s);
+void puts2(char *str);
