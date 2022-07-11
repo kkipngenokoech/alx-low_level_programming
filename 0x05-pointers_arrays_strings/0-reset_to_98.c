74c5ddb604f56a639fadc8cae5a2efea89c29929
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-11 00:56:35 -0700

    introduction of pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - changes the value of n to 98 using pointers
+ * @*n: paramter passed so as to be used to change the value
+ * Return: void nothing to be returned
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this directory is about pointers and arrays
