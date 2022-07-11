16d05f2155299053da7cae0045e5d6b05c5b7b28
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-11 04:50:06 -0700

    printing arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - function to print array
+ * @a: array parameter to be passed
+ * @n: number of elements to be printed in an array
+ */
+void print_array(int *a, int n)
+{
+	int counter;
+
+	for (counter = 0; counter <= n; counter++)
+	{
+		printf("%d, ",a[counter]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,3 +7,4 @@ void rev_string(char *s);
 void print_rev(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
