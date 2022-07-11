b76b5ef1fdde1781cfd1a87f91c5d57228387283
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-11 05:00:55 -0700

    fixing the logic incrimentation of the code

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,9 +9,11 @@ void print_array(int *a, int n)
 {
 	int counter;
 
-	for (counter = 0; counter <= n; counter++)
+	for (counter = 0; counter < n; counter++)
 	{
-		printf("%d, ",a[counter]);
+		printf("%d",a[counter]);
+		if (counter != (n-1))
+			printf(", ");
 	}
 	_putchar('\n');
 }
