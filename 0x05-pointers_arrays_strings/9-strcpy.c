6b00f41f2e044dca5c0c520c557ff2621de239d4
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-11 05:37:55 -0700

    added return statement

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -15,5 +15,6 @@ char *_strcpy(char *dest, char *src)
 		dest[counter] = *src[counter];
 		counter++;
 	}
-	_putchar('\0');
+	dest[counter] = '\0';
+	return (dest);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,3 +8,4 @@ void print_rev(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
