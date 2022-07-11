fe004b8fd01ef966024b87c19a5fb9d686f860ba
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-11 05:31:45 -0700

    copying strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcpy - coping string to another variable
+ * @dest: parameter to be copied to
+ * @src: parameter to be copied from
+ * Return: it returns a string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int counter;
+
+	while (*src[counter] != '\0')
+	{
+		dest[counter] = *src[counter];
+		counter++;
+	}
+	_putchar('\0');
+}
