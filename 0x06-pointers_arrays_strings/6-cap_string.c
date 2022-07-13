746e9618171ba72813f9f925e5786e91a040cd13
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-13 12:14:30 -0700

    cap the strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * cap_string - capitalizes all string letters
+ * @str: string passed into the function
+ * Return: returns the capitalized string
+ */
+char *cap_string(char *str)
+{
+	int counter;
+
+	counter = 0;
+	while (str[counter] != '\0')
+	{
+		if (counter == 0 && (str[counter] >= 'a' && str[counter] <= 'z'))
+		{
+			str[counter] -= 32;
+		}
+		if (str[counter] == ' ' || str[counter] == '\t' || str[counter] == '\n' || str[counter] == ',' || str[counter] == ';' || str[counter] == '.' || str[counter] == '!' || str[counter] == '?' || str[counter] == '"' || str[counter] == '(' || str[counter] == ')' || str[counter] == '{' || str[counter] == '}')
+		{
+			if (str[counter] >= 'a' && str[counter] <= 'z')
+				str[counter] -= 32;
+		}
+		counter++;
+	}
+	return (str);
+}
+
