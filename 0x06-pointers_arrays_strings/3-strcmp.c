2edc53ce3ba097be03121c64adfef17d8333620f
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-13 11:22:43 -0700

    comparing two values

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcmp - it compares two strings
+ * @s1: first string passed
+ * @s2: second string passed
+ * Return: returns zero if the two are equal else another number
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int counter, checker;
+
+	counter = 0;
+	checker = 0;
+	while (checker == 0)
+	{
+		if(s1[counter] == '\0' || s2[counter] == '\0')
+			break;
+		else
+		{
+			checker = s1[counter] - s2[counter];
+			counter++;
+		}
+	}
+	return (checker);
+}
