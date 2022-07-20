aa9243571e4bfc9398efc11931062be5f575bcff
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-20 00:01:11 -0700

    added the logic of 2-str_len recursion ued to find the length of the string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen_recursion - prints the length of the string
+ * @s: string to be couted
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int counter;
+
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s);
+}
