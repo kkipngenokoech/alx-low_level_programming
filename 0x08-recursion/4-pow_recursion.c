9f3298a18ff75296c4d46444c298eae0309e1b06
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-20 02:29:46 -0700

    changed the logic of the file number 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,15 +2,15 @@
 #include "main.h"
 /**
  * _pow_recursion - it returns the value of X raised to power of Y
- * @s: first number that will be powered to
+ * @x: first number that will be powered to
  * @y: number to will be used to power
  * Return: int results
  */
-int _pow_recursion(int x,int y)
+int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return (0);
+		return (-1);
 	if (y == 0)
 		return (1);
-	return (x * _pow_recursion(x, y-1));
+	return (x * _pow_recursion(x, y - 1));
 }
