827fbbd074ece0a6d5f455ff8a747b0831af7497
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-19 23:23:03 -0700

    removed the while loop and used an if else statement which calls itself

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,9 +7,13 @@
  */
 void _puts_recursion(char *s)
 {
-	while (*s != '\0')
+	if (*s != '\0')
 	{
 		_putchar(*s);
+		_puts_recursion(s+1);
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
