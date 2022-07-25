b9b4cea7fc7cafe2fa6738ee3eeea104c6cf4e98
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-25 10:10:13 -0700

    fixed betty documentation

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _strdup - function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
+ * _strdup - function that returns a pointer to a newly allocated space in memory
  * @str: string
  * Return: pointer of an array of chars
  */
