581fcb0e8ee592aa163e6ca02b4165a9998038bf
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-26 09:57:10 -0700

    added the syntax of creating grids

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * alloc_grid - returns a pointer to a 2-dimensional array of integers
+ * @width: width of the array
+ * @height: height of the array
+ * Return: apointer to an array of integers
+ */
+int **alloc_grid(int width, int height)
+{
+	int **printgrid;
+	int index, column;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+	printgrid = malloc(height * sizeof(int *));
+	if (printgrid == NULL)
+	{
+		free(printgrid);
+		return (NULL);
+	}
+	for (index = 0; index < height; index++)
+		for (column = 0; column < width; column++)
+			printgrid[index][column] = 0;
+	return (printgrid);
+}
