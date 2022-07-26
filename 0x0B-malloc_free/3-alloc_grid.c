e47a59b2df630fa3ea9df7852dd8e471f9731b59
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-26 10:23:18 -0700

    made an incriment to the snytax

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,6 +20,17 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 	for (index = 0; index < height; index++)
+	{
+		printgrid[index] = malloc(width * sizeof(int));
+		if (printgrid == NULL)
+		{
+			for (index--; index >= 0;index--)
+				free(printgrid[index]);
+			free(printgrid);
+			return (NULL);
+		}
+	}
+	for (index = 0; index < height; index++)
 		for (column = 0; column < width; column++)
 			printgrid[index][column] = 0;
 	return (printgrid);
