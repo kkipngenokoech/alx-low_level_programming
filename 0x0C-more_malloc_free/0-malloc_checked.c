a6c8dac71bda59dd7434940ede2f73ac2e773145
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-28 09:27:14 -0700

    added the first file

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memeory using malloc
+ * @b: bytes allocated
+ * Return: pointer to first place
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <stdlib.h>
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
