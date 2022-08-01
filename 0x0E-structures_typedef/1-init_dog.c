5f185465d07519d1a3506c19568397f1805c9bdc
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-01 09:37:48 -0700

    added a function that refrences and initalizes the dog struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - initializes a structure of the type dog
+ * @d:array of struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description: this is a dog structure
+ */
+int init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner
+	}
+}
