b6f09f4d4182eac040bef7c1af2d778c72929f33
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-01 09:29:03 -0700

    betty coding style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,4 +19,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+#endif/*dog.h*/
